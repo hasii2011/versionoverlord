69c054f1b2c25e8b566bb73618727c7d77332dc0
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2023-02-25 15:00:55 -0700

    <Unit Tests>[]: <Packages First>
    [Prepare for nascent unit tests]
    
    []

---
+++ versionoverlord/setup/HandleSetupPy.py
@@ -0,0 +1,33 @@
+
+from logging import Logger
+from logging import getLogger
+
+from os import environ as osEnvironment
+
+from versionoverlord.Common import ENV_PROJECT
+from versionoverlord.Common import ENV_PROJECTS_BASE
+from versionoverlord.exceptions.ProjectNotSetException import ProjectNotSetException
+from versionoverlord.exceptions.ProjectsBaseNotSetException import ProjectsBaseNotSetException
+
+
+STD_FILE_NAME: str = 'setup.py'
+
+
+class HandleSetupPy:
+    """
+    Handles the setup.py file
+    """
+    def __init__(self):
+        self.logger: Logger = getLogger(__name__)
+
+        try:
+            self._projectsBase: str = osEnvironment[ENV_PROJECTS_BASE]
+        except KeyError:
+            raise ProjectsBaseNotSetException
+        try:
+            self._projectDirectory: str = osEnvironment[ENV_PROJECT]
+        except KeyError:
+            raise ProjectNotSetException
+
+    def exists(self) -> bool:
+        return True
